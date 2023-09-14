235c57732b8b2756d9a0552dd2ce0f1fa4938587
NestorDanielGomez <nestordanielgomez@gmail.com>
2023-09-14 09:48:51 -0300

    refactory y proxy funcionando

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,20 +1,16 @@
 require("dotenv").config();
-const puppeteer = require("puppeteer");
-const sessionFactory = require("./factories/sessionFactory");
-const userFactory = require("./factories/userFactory");
 
-let browser, page;
+const Page = require("./helpers/page");
+
+let page;
 
 beforeEach(async () => {
-  browser = await puppeteer.launch({
-    headless: false,
-  });
-  page = await browser.newPage();
+  page = await Page.build();
   await page.goto("localhost:3000");
 });
 
 afterEach(async () => {
-  await browser.close();
+  await page.close();
 });
 
 test("Titulo del Header correcto", async () => {
@@ -30,13 +26,7 @@ test("Click en login empieza el proceso de oauth", async () => {
 });
 
 test("Click me logueo, debe mostrar el boton de cerrar sesión", async () => {
-  const user = await userFactory();
-  const { session, sig } = sessionFactory(user);
-
-  await page.setCookie({ name: "session", value: session });
-  await page.setCookie({ name: "session.sig", value: sig });
-  await page.goto("localhost:3000");
-  await page.waitFor(`a[href="/auth/logout"]`);
+  await page.login();
 
   const text = await page.$eval(`a[href="/auth/logout"]`, (el) => el.innerHTML);
   expect(text).toEqual("Cerrar Sesión");
